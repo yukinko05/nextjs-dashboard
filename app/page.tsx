f16d523f203afaf2f1dddbef78be6141dd5afbbc
yukinko05 <grey.sora.35@gmail.com>
2024-12-08 04:12:12 +0900

    デバイスごとに異なるヒーローイメージを表示する機能を実装
    - Imageコンポーネントを使用してデバイスごとに異なる画像を表示
    - デスクトップとモバイルに対応した画像を設定

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import '@/app/ui/global.css';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from "@/app/ui/fonts"
+import Image from "next/image";
 
 export default function Page() {
   return (
@@ -30,6 +31,19 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className='hidden md:block'
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version" />
         </div>
       </div>
     </main>
