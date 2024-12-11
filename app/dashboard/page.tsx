d5f62a0972dc0ead8687365be1e6d422b81d3607
yukinko05 <grey.sora.35@gmail.com>
2024-12-11 18:38:24 +0900

    <RevenueChart/>コンポーネントのデータを取得して画面に表示する機能を実装

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,24 @@
-export default function DashboardPage() {
-  return <p>Dashboard Page</p>
+import { Card } from "@/app/ui/dashboard/cards";
+import RevenueChart from "../ui/dashboard/revenue-chart";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
+import { lusitana } from "../ui/fonts";
+import { fetchRevenue } from "@/app/lib/data";
+
+export default async function DashboardPage() {
+  const revenue = await fetchRevenue();
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* <card title="collected" value={totalPaidInvoices} type="collected /> */}
+        {/* <card title="pending" value={totalPendingInvoices} type="pending /> */}
+        {/* <card title="Total Invoices" value={numberOfInvoices} type="invoices /> */}
+        {/* <card title="Total Customers" value={numberOfCustomers} type="customers" /> */}
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+      </div>
+    </main>
+  )
 }
