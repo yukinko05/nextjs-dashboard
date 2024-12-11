748cea30c006df8b421289e37c8a0f82ffe5aaff
yukinko05 <grey.sora.35@gmail.com>
2024-12-11 18:47:10 +0900

    <LatestInvoices />コンポーネントのデータを取得して画面に表示する機能を実装

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,10 +2,11 @@ import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "../ui/dashboard/revenue-chart";
 import LatestInvoices from "../ui/dashboard/latest-invoices";
 import { lusitana } from "../ui/fonts";
-import { fetchRevenue } from "@/app/lib/data";
+import { fetchRevenue, fetchLatestInvoices } from "@/app/lib/data";
 
 export default async function DashboardPage() {
   const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
@@ -17,7 +18,7 @@ export default async function DashboardPage() {
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue} />
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   )
