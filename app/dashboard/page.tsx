55585e82295b29a475b0a75d54cc15ed1de7b0a9
yukinko05 <grey.sora.35@gmail.com>
2024-12-12 18:40:31 +0900

    fetchCardData関数からデータを取得してCardコンポーネントを画面に表示する実装

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,19 +2,21 @@ import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "../ui/dashboard/revenue-chart";
 import LatestInvoices from "../ui/dashboard/latest-invoices";
 import { lusitana } from "../ui/fonts";
-import { fetchRevenue, fetchLatestInvoices } from "@/app/lib/data";
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from "@/app/lib/data";
 
 export default async function DashboardPage() {
   const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
+  const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
+
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <card title="collected" value={totalPaidInvoices} type="collected /> */}
-        {/* <card title="pending" value={totalPendingInvoices} type="pending /> */}
-        {/* <card title="Total Invoices" value={numberOfInvoices} type="invoices /> */}
-        {/* <card title="Total Customers" value={numberOfCustomers} type="customers" /> */}
+        <Card title="collected" value={totalPaidInvoices} type="collected" />
+        <Card title="pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card title="Total Customers" value={numberOfCustomers} type="customers" />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue} />
