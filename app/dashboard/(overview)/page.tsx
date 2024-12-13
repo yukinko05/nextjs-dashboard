a056e9069a5107d458b0fa27cbb634034a20a9bc
yukinko05 <grey.sora.35@gmail.com>
2024-12-13 17:23:40 +0900

    新規フォルダを作成しLoadingとダッシュボード概要ファイルを格納
    - ダッシュボード概要にのみLoadingコンポーネントを適用させるためにURLパス構造に影響のない()を使って作成

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,7 +1,7 @@
 import { Card } from "@/app/ui/dashboard/cards";
-import RevenueChart from "../ui/dashboard/revenue-chart";
-import LatestInvoices from "../ui/dashboard/latest-invoices";
-import { lusitana } from "../ui/fonts";
+import RevenueChart from "../../ui/dashboard/revenue-chart";
+import LatestInvoices from "../../ui/dashboard/latest-invoices";
+import { lusitana } from "../../ui/fonts";
 import { fetchRevenue, fetchLatestInvoices, fetchCardData } from "@/app/lib/data";
 
 export default async function DashboardPage() {
