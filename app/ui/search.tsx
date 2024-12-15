7c2b2289336f00c32fa4e73f6291fb274de1fd31
yukinko05 <grey.sora.35@gmail.com>
2024-12-15 20:06:54 +0900

    useRouterとusePathnameフックを使用してURLを更新する機能を実装

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchPrams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchPrams);
@@ -13,7 +15,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     } else {
       params.delete("query");
     }
-    console.log(params)
+    replace(`${pathname}?${params.toString()}`);
   }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
