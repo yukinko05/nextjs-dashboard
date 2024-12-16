e4d3b118358a425205664b0024cdf5aa941c2623
yukinko05 <grey.sora.35@gmail.com>
2024-12-16 17:15:05 +0900

    use-debounceをインストールしてキー入力ごとに新しいデータベースクエリが実行されないように修正

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,13 +2,15 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchPrams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchPrams);
     if (term) {
       params.set("query", term);
@@ -16,7 +18,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete("query");
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 1000);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
