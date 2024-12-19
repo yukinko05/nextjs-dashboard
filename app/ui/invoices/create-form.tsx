7f8a5fb0b4a3db0e95167e5d98516d94ea3f548d
yukinko05 <grey.sora.35@gmail.com>
2024-12-19 17:04:38 +0900

    ariaラベルを追加し、未入力時にエラーメッセージを表示する機能を実装

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -31,6 +31,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
+              aria-describedby="customer-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -46,6 +47,11 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
         </div>
 
         {/* Invoice Amount */}
+        <div id="customer-error" aria-live="polite" aria-atomic="true">
+          {state.errors?.customerId && state.errors.customerId.map((error: string) => (
+            <p className="mt-2 text-sm text-red-500" key={error}>{error}</p>
+          ))}
+        </div>
         <div className="mb-4">
           <label htmlFor="amount" className="mb-2 block text-sm font-medium">
             Choose an amount
@@ -66,6 +72,11 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
         </div>
 
         {/* Invoice Status */}
+        <div id="amount-error" aria-live="polite" aria-atomic="true">
+          {state.errors?.amount && state.errors.amount.map((error: string) => (
+            <p className="mt-2 text-sm text-red-500" key={error}>{error}</p>
+          ))}
+        </div>
         <fieldset>
           <legend className="mb-2 block text-sm font-medium">
             Set the invoice status
@@ -103,6 +114,11 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 </label>
               </div>
             </div>
+          </div>
+          <div id="status-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.status && state.errors.status.map((error: string) => (
+              <p className="mt-2 text-sm text-red-500" key={error}>{error}</p>
+            ))}
           </div>
         </fieldset>
       </div>
