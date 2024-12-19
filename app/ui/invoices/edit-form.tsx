a3049f6e33fc0638d325c4d8bde4bc9bd89a0286
yukinko05 <grey.sora.35@gmail.com>
2024-12-19 23:21:23 +0900

    請求書編集ページのエラーの状態管理と表示機能を実装

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,7 +9,8 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-import { updateInvoice } from '@/app/lib/actions';
+import { updateInvoice, State } from '@/app/lib/actions';
+import { useActionState } from 'react';
 
 export default function EditInvoiceForm({
   invoice,
@@ -18,11 +19,13 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-
+  const initialState: State = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
 
+  console.log(state)
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -35,6 +38,7 @@ export default function EditInvoiceForm({
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={invoice.customer_id}
+              aria-activedescendant="customer-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -48,7 +52,16 @@ export default function EditInvoiceForm({
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
         </div>
-
+        <div id="customer-error" aria-live="polite" aria-atomic="true">
+          {state.errors?.customerId && state.errors.customerId.map((error: string) => (
+            <p className="mt-2 text-sm text-red-500" key={error}>{error}</p>
+          ))}
+        </div>
+        <div id="customer-error" aria-live="polite" aria-atomic="true">
+          {state.errors?.customerId && state.errors.customerId.map((error: string) => (
+            <p className="mt-2 text-sm text-red-500" key={error}>{error}</p>
+          ))}
+        </div>
         {/* Invoice Amount */}
         <div className="mb-4">
           <label htmlFor="amount" className="mb-2 block text-sm font-medium">
@@ -69,7 +82,11 @@ export default function EditInvoiceForm({
             </div>
           </div>
         </div>
-
+        <div id="amount-error" aria-live="polite" aria-atomic="true">
+          {state.errors?.amount && state.errors.amount.map((error: string) => (
+            <p className="mt-2 text-sm text-red-500" key={error}>{error}</p>
+          ))}
+        </div>
         {/* Invoice Status */}
         <fieldset>
           <legend className="mb-2 block text-sm font-medium">
@@ -111,6 +128,15 @@ export default function EditInvoiceForm({
               </div>
             </div>
           </div>
+          <div id="status-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.status && state.errors.status.map((error: string) => (
+              <p className="mt-2 text-sm text-red-500" key={error}>{error}</p>
+            ))}
+          </div>
+          {state.message &&
+            <div id="edit-error" aria-live="polite" aria-atomic="true">
+              <p className="mt-2 text-sm text-red-500">{state.message}</p>
+            </div>}
         </fieldset>
       </div>
       <div className="mt-6 flex justify-end gap-4">
