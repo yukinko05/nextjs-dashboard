b63eecf01ec1253d65bdb29e1a9b3546ed5f0602
yukinko05 <grey.sora.35@gmail.com>
2024-12-20 05:47:05 +0900

    認証情報プロバイダーを追加

---
+++ auth.ts
@@ -0,0 +1,8 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+import credentials from "next-auth/providers/credentials";
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [credentials({})],
+});
