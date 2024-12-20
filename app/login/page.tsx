43fbd89b4ad22d0fd39371267a00fb0c61909464
yukinko05 <grey.sora.35@gmail.com>
2024-12-20 17:18:56 +0900

    各ページのmetaデータタイトルを追加

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,13 @@
 import AcmeLogo from "../ui/acme-logo";
 import LoginForm from "../ui/login-form";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Login',
+};
 
 export default function LoginPage() {
+
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,11 @@ import '@/app/ui/global.css';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from "@/app/ui/fonts"
 import Image from "next/image";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default function Page() {
   return (
